4a5d78cbd7e309c5e1d945e260b93dba47928335
GLOBANT\taidy.marrugo <taidy.marrugo@globant.com>
2019-01-16 14:44:35 -0500

    remove open from variable activity from ActivityView

--- app/src/main/java/com/globant/counter/mvp/view/ActivityView.kt
+++ app/src/main/java/com/globant/counter/mvp/view/ActivityView.kt
@@ -9,7 +9,7 @@ import java.lang.ref.WeakReference
 open class ActivityView(activity: Activity) {
     private val activityRef: WeakReference<Activity> = WeakReference(activity)
 
-    open  val activity: Activity?
+      val activity: Activity?
         get() = activityRef.get()
 
 
